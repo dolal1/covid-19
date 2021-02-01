96da1bf56c988cb4f2cf00173be78825b4c7b715
dolal1 <52042659+dolal1@users.noreply.github.com>
2021-02-01 14:13:40 +0300

    Healthworker Model added

---
+++ app/Models/Healthworker.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Healthworker extends Model
+{
+    use HasFactory;
+
+    public function hospital () {
+        return $this->belongsTo('App\Models\Hospital');
+    }
+
+    public function patients () {
+        return $this->hasMany('App\Models\Patient');
+    }
+}
